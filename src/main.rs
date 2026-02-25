fcdc2adfc85dee3b380c6d7b91f4c465ec6e9d31
Joe Howarth <josephehowarth@gmail.com>
2026-02-25 16:04:59 -0700

    default viewer logging to info for easier v0 debugging

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ fn parse_tint(input: &str) -> Result<[f32; 4], String> {
 }
 
 fn main() -> Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let cli = Cli::parse();
 
     let data_dir = resolve_data_dir(&cli.rimworld_data)
